d039eec53ac327d61997b21612df9ce90174aca3
Andy-i3 <interwebandy@gmail.com>
2019-06-19 10:24:55 +0300

    new 1

--- src/main/java/rushydro/ru/Renamefiles.java
+++ src/main/java/rushydro/ru/Renamefiles.java
@@ -2,93 +2,103 @@ package rushydro.ru;
 
 //  @author Golubkov Andrey 12-07-2018 ver 1.0 beta
 //  Переименование файлов
-// @version 0.1
+// @version 0.2
 // @sin 0.1
-
-
-// import com.sun.org.apache.xpath.internal.operations.String;
 
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.stream.Collectors;
 import java.util.List;
-import java.lang.*;
+import java.util.stream.Collectors;
 
 public class Renamefiles {
 
-    private static String s2;
-    private static int i;
+    private static String currentNameFile;
+    private static int deep_scan;
     private static String path = null;
 
+
     public static void main(String[] args) {
-
         String str = args[0];
-
-        String[] pArray = null;
-
         if (str.length() > 0) {
-            pArray = str.split("\\\\");
+            String[] pArray = str.split("\\\\");
             path = str;
+            deep_scan = pArray.length; // Глубина сканирования
         } else {
             System.out.println( "No directory with files to process" );
             System.exit( 0 );
         }
-
-        List<File> files = scanNameFiles( path , pArray.length);
-        new Renamefiles( files );
+        new Renamefiles( scanNameFiles( path ));
         System.out.println( "Successfully completed" );
     }
 
     // Сканирование директории
-    // @return - список файлов
-     public static List<File> scanNameFiles (String path, int i) {
-         List <File> files = null;
-         try {
-             files = Files.walk( Paths.get( path ), i ).filter( Files::isRegularFile ).map( Path::toFile ).collect( Collectors.toList() );
-         }
-         // Обработка ошибок
-         catch (StringIndexOutOfBoundsException sio) {
-             System.out.println( "Invalid file format: " + s2 );
-             System.exit( 0 );
-         } catch (Exception ex) {
-             System.out.println( "Error: " + ex );
-             System.exit( 0 );
-         }
-         return files;
-     }
-
-   // фильтрация и переименование файлов по маске .00. и JPG
-     private Renamefiles(List <File> collect ) {
-
-       // Цикл по коллекции и модефикация названия файлов
-       for (File file : collect) {
-           String s = file.getAbsolutePath();
-           s2 = file.getName();
-           String[] pars = s.split( "\\\\" );
-           if ((s2.replaceAll( "^.*\\.(.*)$", "$1" ).equals( "jpg" ))) {
-               if ((s2.substring( s2.length() - 10, s2.length() - 6 )).equals( ".00." )) {
-                   String s3 = s2.substring( pars[i].length() );
-                   s3 = s3.replace( "(", "" );
-                   s3 = s3.replace( ")", "" );
-                   s3 = s3.replaceAll( "^\\s+", "" ); // Удалить пробелы в начале строки
-                   s3 = s3.substring( 0, 1 ).toUpperCase() + s3.substring( 1 );
-                   String s4 = (s3.substring( s3.length() - 23 ));
-                   s4 = s4.replace( ".jpg", "" );
-                   s4 = s4.replace( ".", "_" );
-                   s4 = s4.replace( "_", "-" );
-                   s4 = s4.substring( 0, 16 );
-                   s3 = s3.substring( 0, s3.length() - 23 );
-                   s3 = s3.replaceAll( "\\s+$", "" );  // Удалить пробелы в конце строки
-                   String s5 = s4 + " " + s3 + ".jpg";
-                   System.out.println( path + "\\" + pars[i] + "\\" + s5 );
-                   boolean b = file.renameTo( new File( path + "\\" + pars[i] + "\\" + s5 ) ); // Переименование файлов
-               }
-           }
-       }
+    // @return - Возвращает список файлов
+    private static List<File> scanNameFiles (String path) {
+        List <File> files = null;
+        try {
+            files = Files.walk( Paths.get( path ), deep_scan ).filter( Files::isRegularFile ).map( Path::toFile ).collect( Collectors.toList() );
+        }
+        // Обработка ошибок
+        catch (StringIndexOutOfBoundsException sio) {
+            System.out.println( "Invalid file format: " + currentNameFile );
+            System.exit( 0 );
+        } catch (Exception ex) {
+            System.out.println( "Error: " + ex );
+            System.exit( 0 );
+        }
+        return files;
+    }
 
 
-   }
+    private static String Renamefile (String s, String currentNameFile, int pars_length) {
+
+                String s3 = currentNameFile.substring( pars_length );
+                s3 = s3.replace( "(", "" );
+                s3 = s3.replace( ")", "" );
+                s3 = s3.replaceAll( "^\\s+", "" ); // Удалить пробелы в начале строки
+                s3 = s3.substring( 0, 1 ).toUpperCase() + s3.substring( 1 );
+                String s4 = (s3.substring( s3.length() - 23 ));
+                s4 = s4.replace( ".jpg", "" );
+                s4 = s4.replace( ".", "_" );
+                s4 = s4.replace( "_", "-" );
+                s4 = s4.substring( 0, 16 );
+                s3 = s3.substring( 0, s3.length() - 23 );
+                s3 = s3.replaceAll( "\\s+$", "" );  // Удалить пробелы в конце строки
+                return s4 + " " + s3 + ".jpg";;
+
+    }
+
+    // фильтрация и переименование файлов по маске .00. и JPG
+    private Renamefiles(List <File> collect ) {
+
+        // Цикл по коллекции и модефикация названия файлов
+        for (File file : collect) {
+            String s = file.getAbsolutePath();
+            currentNameFile = file.getName();
+            String[] pars = s.split( "\\\\" );
+
+        if((currentNameFile.replaceAll("^.*\\.(.*)$","$1").equals("jpg"))){
+         if((currentNameFile.substring(currentNameFile.length()-10,currentNameFile.length()-6)).equals(".00.")){
+                 String filename=Renamefile(s,currentNameFile,pars[deep_scan].length());
+             }
+        }
+
+
+            System.out.println( path + "\\" + pars[deep_scan] + "\\" + filename );
+            try {
+                boolean b = file.renameTo( new File( path + "\\" + pars[deep_scan] + "\\" + filename ) ); // Переименование файлов
+            }
+            // Обработка ошибок
+            catch (StringIndexOutOfBoundsException sio) {
+                System.out.println( "Invalid file format: " + currentNameFile );
+                System.exit( 0 );
+            } catch (Exception ex) {
+                System.out.println( "Error: " + ex );
+                System.exit( 0 );
+            }
+        }
+    }
 }
 
---
+++ src/test/java/rushydro/ru/RenamefilesTest.java
@@ -0,0 +1,15 @@
+package rushydro.ru;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RenamefilesTest {
+
+    @Test
+    public void main() {
+
+      //  "загорская гаэс-2 (строительная площадка 1) 17_05_2019 11.00.00.jpg"
+
+    }
+}
