4d02daea08d70708ab3433a304f0935ea2198aae
Andy-i3 <interwebandy@gmail.com>
2019-06-20 11:30:38 +0300

    new ver 2.1

--- src/main/java/rushydro/ru/Renamefiles.java
+++ src/main/java/rushydro/ru/Renamefiles.java
@@ -1,12 +1,19 @@
 package rushydro.ru;
 
-//  @author Golubkov Andrey 12-07-2018
-//  Переименование файлов
-// @version 1.3
-// @sin 0.1
-
-
-
+/**
+ * Программа выполняет переименование файлов в дереве директорий согласно заданным правилам
+ * <p>
+ * Например:
+ * <p>
+ * Input: загорская гаэс-2 (строительная площадка 2) 30_11_2018 09.23.12.jpg
+ * Output:   Загорская ГАЭС-2\30-11-2018 09-23 Строительная площадка 2.jpg
+ * <p>
+ * Если в имени файла нет имени диектории, то файл формируется с названием.
+ *
+ * @author Golubkov Andrey 12-07-2018
+ * @version 2.1
+ * @sin 0.1
+ */
 
 import java.io.File;
 import java.nio.file.Files;
@@ -16,36 +23,47 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Renamefiles {
+    private static String Test = "";
 
-    // Переименование файлов
-    private Renamefiles(List <File> collect) {
+    /**
+     * Переименование файлов
+     * @param collect - Массив с файловыми объектами для обработки.
+     */
+    public static void RenameAllfiles(List <File> collect) {
 
         // Цикл по коллекции и модефикация названия файлов
         for (File file : collect) {
             String currentNameFile = file.getName();
-            // фильтрация и переименование файлов по маске .00. и JPG
+            /**
+             *
+             * фильтрация и переименование файлов по маске DD (цифры) и JPG
+             */
             if ((currentNameFile.replaceAll( "^.*\\.(.*)$", "$1" ).equals( "jpg" ))) {
                 if ((currentNameFile.substring( currentNameFile.length() - 9, currentNameFile.length() - 7 )).matches( ".*[0-9].*[0-9]" )) {
-
                     String currentNamePatch = file.getAbsolutePath();
                     currentNamePatch = currentNamePatch.substring( 0, currentNamePatch.length() - currentNameFile.length() - 1 );
                     String[] pars = currentNamePatch.split( "\\\\" );
-
                     String filename = null;
+
+                    /**
+                     *
+                     * Проверка соответствия названия директории и файла.
+                     * При совпадении название директории вычетается из названия файла
+                     *
+                     */
+
                     String f1 = pars[pars.length - 1];
                     if ((currentNameFile.substring( 0, f1.length() ).toLowerCase().equals( f1.toLowerCase() ))) {
-                         filename = RenameOnefile( currentNameFile, f1.length() );
+                        filename = RenameOnefile( currentNameFile, f1.length() );
                     } else {
-                         filename = RenameOnefile( currentNameFile, 0 );
+                        filename = RenameOnefile( currentNameFile, 0 );
                     }
-
                     try {
-
-                        // boolean b = file.renameTo( new File( currentNamePatch  + "\\" + filename  ) ); // Переименование файлов
+                         if (!Test.equals( "test" )) {
+                           file.renameTo( new File( currentNamePatch + "\\" + filename ) ); // Переименование файлов
+                         }
                         System.out.println( currentNamePatch + "\\" + filename ); // Переименование файлов
-                    }
-                    // Обработка ошибок
-                    catch (StringIndexOutOfBoundsException sio) {
+                    } catch (StringIndexOutOfBoundsException sio) {
                         System.out.println( "Invalid file format: " + currentNameFile );
                         System.exit( 0 );
                     } catch (Exception ex) {
@@ -57,29 +75,51 @@ public class Renamefiles {
         }
     }
 
+    /**
+     * When {@parm } is null, program not run
+     *
+     *
+     * @param args - Путь к директории с файлами для обработки
+     * @throws Exception
+     * @exception - Если нет пути к директории и другая ошибка
+     */
     public static void main(String[] args) throws Exception {
         try {
-            String str = args[0];
-            String pathToDirectory = null;
-            String[] pArray = str.split( "\\\\" );
-            pathToDirectory = str; // Путь к директории с файлами
-            new Renamefiles( scanNameFiles( pathToDirectory, pArray.length ) );
-            System.out.println( "Successfully completed" );
-        } catch (Exception e) {
+            if (args.length != 0) {
+                String str = args[0];
+                String pathToDirectory = null;
+                String[] pArray = str.split( "\\\\" );
+                pathToDirectory = str; // Путь к директории с файлами
+                RenameAllfiles( scanNameFiles( pathToDirectory, pArray.length ) );
+                System.out.println( "Successfully completed" );
+                if (args.length > 1) {
+                    Test = args[1];
+                }
+            }
+          else
+            {
             System.out.println( "The path to the file directory is not defined." );
+            System.exit( 0 );
+            }
+    }
+        catch (Exception ex) {
+            System.out.println( "Error: " + ex );
+            System.exit( 0 );
         }
     }
 
-    // Сканирование директории
-    // deep_scan; - Глубина сканирования директории
-    // @return - Возвращает список файлов с директориями (полный путь)
+    /**
+     * Сканирование директории
+     * @param path - Путь к директории с файлами
+     * @param deep_scan - Глубина сканирования директории
+     * @return Возвращает список файлов с директориями (полный путь)
+     */
+
     public static List <File> scanNameFiles(String path, int deep_scan) {
         List <File> files = null;
         try {
             files = Files.walk( Paths.get( path ), deep_scan ).filter( Files::isRegularFile ).map( Path::toFile ).collect( Collectors.toList() );
-        }
-        // Обработка ошибок
-        catch (StringIndexOutOfBoundsException sio) {
+        } catch (StringIndexOutOfBoundsException sio) {
             System.out.println( "Invalid file format: " );
             System.exit( 0 );
         } catch (Exception ex) {
@@ -89,14 +129,16 @@ public class Renamefiles {
         return files;
     }
 
-    // Переименование имени файла
-    // Input: загорская гаэс-2 (строительная площадка 1) 17_05_2019 11.00.00.jpg
-    // Output: 17-05-2019 11-00 Загорская гаэс-2 строительная площадка 1.jpg
-    // @return
+    /**
+     * Переименование имени файла
+     * @param nameFile Example: загорская гаэс-2 (строительная площадка 1) 17_05_2019 11.00.00.jpg
+     * @param ColStr - Количество символов директории, если 0 то название директрии в название файла.
+     * @return - Example: 17-05-2019 11-00 Загорская гаэс-2 строительная площадка 1.jpg
+     */
+
     public static String RenameOnefile(String nameFile, int ColStr) {
-        if (ColStr != 0 && ColStr>5) {
-            nameFile = nameFile.substring( 0, ColStr );
-            System.out.println( nameFile );
+        if (ColStr != 0) {
+            nameFile = nameFile.substring( ColStr, nameFile.length() );
         }
         nameFile = nameFile.replace( "(", "" ); // Удалить скобки
         nameFile = nameFile.replace( ")", "" ); // Удалить скобки
--- src/test/java/rushydro/ru/RenamefilesTest.java
+++ src/test/java/rushydro/ru/RenamefilesTest.java
@@ -7,11 +7,16 @@ import static org.junit.Assert.assertEquals;
 public class RenamefilesTest {
 
     @Test
-    public void renameOnefile() {
+    public void renameOnefile_FullPatch() {
+        String result = Renamefiles.RenameOnefile( "загорская гаэс-2 (строительная площадка 2) 30_11_2018 09.23.12.jpg", 16 );
+        String parse = "30-11-2018 09-23 Строительная площадка 2.jpg";
+        assertEquals( result, parse );
+    }
 
+    @Test
+    public void renameOnefileShortPatch() {
         String result = Renamefiles.RenameOnefile( "загорская гаэс-2 (строительная площадка 1) 17_05_2019 11.00.00.jpg", 0 );
         String parse = "17-05-2019 11-00 Загорская гаэс-2 строительная площадка 1.jpg";
         assertEquals( result, parse );
-
     }
 }
